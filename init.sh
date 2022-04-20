910345ad4c29dde989faefa855b95cfb4c91fa8d
Ubuntu <aziztaha@aziz-test-vm.zj5mx5r5k4nuvmpfn2sqtutuug.ax.internal.cloudapp.net>
2022-04-20 15:59:59 +0000

    add nifi

---
+++ init.sh
@@ -0,0 +1,8 @@
+
+DRIVER_NAME=postgresql-42.3.4.jar
+sudo mkdir /datalake/
+sudo mkdir /opt/datalake/nifi/
+sudo mkdir /opt/datalake/nifi/drivers
+sudo mkdir /opt/datalake/nifi/data
+sudo wget https://jdbc.postgresql.org/download/$DRIVER_NAME
+sudo mv $DRIVER_NAME /opt/datalake/nifi/drivers/$DRIVER_NAME
--- startup.sh
+++ startup.sh
@@ -7,3 +7,4 @@ hadoop fs -chmod g+w   /user/hive/warehouse
 
 cd $HIVE_HOME/bin
 ./hiveserver2 --hiveconf hive.server2.enable.doAs=false
+

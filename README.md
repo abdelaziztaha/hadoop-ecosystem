0817871bfd0ace679cb1cbb103e1ca038d42f9ab
Ubuntu <aziztaha@aziz-test-vm.zj5mx5r5k4nuvmpfn2sqtutuug.ax.internal.cloudapp.net>
2022-05-10 08:47:35 +0000

    adding atlas

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ This Big Data Ecosystem is still not complete, but can already be run and tested
 
 **To run:**
 
-3- Execute the init.sh bash script
+3- Execute the init.sh bash script to prepare directory structure on your lacal machine for persisting container volumes. This script is to be only once before the first run. Reruning it later has no effect.
 
 2- run: docker-compose up
 
-**To Test (wait until docker-compose completely run, it can take up 5 min):**
+**To Test (wait until docker-compose completely run, it can take up 15 min):**
 
 ***Testing Hadoop:*** 
 
@@ -32,3 +32,11 @@ docker-compose exec hive-server bash
 Open the browser with http://localhost:9999 to test nifi (Note that the jdbc drivers for the hive metastor are already saved in the container in the mounted directory /opt/datalake/nifi/drivers/postgresql-42.3.4.jar) 
 
 !! Note that your settings in NiFi will be lost after restarting the container. A fix for this is comming soon.
+
+***Testing ATLAS:***
+
+Open the browser with http://localhost:21000 to test ATLAS. Login with admin/admin
+
+***Testing HUE:***
+
+Open the browser with http://localhost:8888 to test HUE. Create a new login to get started
--- init.sh
+++ init.sh
@@ -4,26 +4,28 @@
 # This is the place to create setings in the host (outside the containers
 # such as common folders, etc.
 
-export VOLUME_ROOT=/home/aziztaha/datalake5
+export VOLUME_ROOT=/home/aziztaha/datalake
 export USER=aziztaha
 echo "Crating directories in "$VOLUME_ROOT
 
 DRIVER_NAME=postgresql-42.3.4.jar
 
-sudo mkdir $VOLUME_ROOT
+mkdir -p $VOLUME_ROOT
 
-sudo mkdir $VOLUME_ROOT/hadoop
-sudo mkdir $VOLUME_ROOT/hadoop/dfs
-sudo mkdir $VOLUME_ROOT/hadoop/dfs/name
-sudo mkdir $VOLUME_ROOT/hadoop/dfs/data
-sudo mkdir $VOLUME_ROOT/meta
-sudo mkdir $VOLUME_ROOT/meta/postgedata
-sudo mkdir $VOLUME_ROOT/drivers
+mkdir -p $VOLUME_ROOT/hadoop
+mkdir -p $VOLUME_ROOT/hadoop/dfs
+mkdir -p $VOLUME_ROOT/hadoop/dfs/name
+mkdir -p $VOLUME_ROOT/hadoop/dfs/data
+mkdir -p $VOLUME_ROOT/meta
+mkdir -p $VOLUME_ROOT/meta/postgredata
+mkdir -p $VOLUME_ROOT/atlas
+mkdir -p $VOLUME_ROOT/atlas/models
+mkdir -p $VOLUME_ROOT/atlas/models/hadoop
+mkdir -p $VOLUME_ROOT/drivers
 
-sudo wget https://jdbc.postgresql.org/download/$DRIVER_NAME
-sudo mv $DRIVER_NAME $VOLUME_ROOT/drivers/$DRIVER_NAME
+wget https://jdbc.postgresql.org/download/$DRIVER_NAME
+mv $DRIVER_NAME $VOLUME_ROOT/drivers/$DRIVER_NAME
 
-sudo chown -R  $USER:$USER $VOLUME_ROOT
-sudo chmod -R 777 $BASE_ROOT
+#chown -R  $USER:$USER $VOLUME_ROOT
 
 
